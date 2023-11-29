687c38945d3dab7a5dfb1cd939724468f02f5d59
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-11-29 11:49:42 +0200

    Using Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
+++ app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.danym.stockinvestingapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,27 +12,67 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.danym.stockinvestingapp.model.StockInfo
 import com.danym.stockinvestingapp.ui.theme.StockInvestingAppTheme
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.http.GET
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
+    private val BASE_URL =
+        "https://financialmodelingprep.com/api/v3/profile/"
+
+    interface StockInfoApi {
+        @GET("AAPL?apikey=QZXaYu1lbCqqIqsBvLBRw1XEtHP7lMo4")
+        fun getStockInfo(): Call<List<StockInfo>>
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StockInvestingAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
         }
+        val executor: Executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            val client = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build()
+            val stockApi = client.create(StockInfoApi::class.java)
+            val result = stockApi.getStockInfo().execute()
+            val stockInfoBody: List<StockInfo>? = result.body()
+
+//            if (stockInfoBody.isNullOrEmpty() || true) {
+//                Toast.makeText(this, "No information about stock: X", Toast.LENGTH_SHORT).show()
+//            }
+            Log.i("test", "first log: ${stockInfoBody?.getOrNull(0)?.ceo}")
+        }
+
+//        thread {
+
+//
+//        }.start()
+
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
