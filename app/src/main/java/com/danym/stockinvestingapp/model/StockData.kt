bdb85f124677a723a459d0545387c64207cc9484
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-12-02 23:06:10 +0200

    Add data class for stock data

---
+++ app/src/main/java/com/danym/stockinvestingapp/model/StockData.kt
@@ -0,0 +1,23 @@
+package com.danym.stockinvestingapp.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class StockData(
+    val date: String,
+    val open: Double,
+    val high: Double,
+    val low: Double,
+    val close: Double,
+    @JsonProperty("adjClose")
+    val adjustedClose: Double,
+    val volume: Long,
+    @JsonProperty("unadjustedVolume")
+    val unadjustedVolume: Long,
+    val change: Double,
+    @JsonProperty("changePercent")
+    val changePercent: Double,
+    val vwap: Double,
+    val label: String,
+    @JsonProperty("changeOverTime")
+    val changeOverTime: Double
+)
