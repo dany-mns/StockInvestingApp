ec504e2fe158b5affa86cde8e2bfcfd17f1b2d05
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-12-02 22:46:28 +0200

    Optimize imports

--- app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
+++ app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
@@ -13,11 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.danym.stockinvestingapp.CompanyProfile.Companion.newIntent
 import com.danym.stockinvestingapp.components.CardComponent
@@ -93,7 +87,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListingScreen(navigateToCompanyProfile: (Stock) -> Unit) {
     Scaffold(content = {
