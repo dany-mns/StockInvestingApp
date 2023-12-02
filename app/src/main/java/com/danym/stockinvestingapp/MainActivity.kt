09225a8e58b3c5c6918d1c7a83f09c48041daf77
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-12-02 19:11:17 +0200

    Dynamic load of resources by name

--- app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
+++ app/src/main/java/com/danym/stockinvestingapp/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.danym.stockinvestingapp
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -31,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.danym.stockinvestingapp.model.Stock
@@ -141,16 +140,23 @@ fun StockListItem(stock: Stock) {
             ) {
                 Text(text = stock.name, style = MaterialTheme.typography.titleLarge)
                 Text(text = stock.ticker, style = MaterialTheme.typography.bodyMedium)
-                Text(text = "69$", style = MaterialTheme.typography.bodySmall)
             }
         }
     }
 }
 
+@SuppressLint("DiscouragedApi")
 @Composable
 fun StockImage(ticker: String) {
+    val context = LocalContext.current
     Image(
-        painter = painterResource(id = R.drawable.msft),
+        painter = painterResource(
+            id = context.resources.getIdentifier(
+                ticker.lowercase(),
+                "drawable",
+                context.packageName
+            )
+        ),
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
