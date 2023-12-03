eca5687c9e0236ac316d6ff901f74d889a6c1198
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-12-03 17:41:33 +0200

    Replace deprecated api for checking the network

--- app/src/main/java/com/danym/stockinvestingapp/viewmodel/StockViewModel.kt
+++ app/src/main/java/com/danym/stockinvestingapp/viewmodel/StockViewModel.kt
@@ -2,7 +2,7 @@ package com.danym.stockinvestingapp.viewmodel
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.danym.stockinvestingapp.data.AppDatabase
@@ -10,6 +10,7 @@ import com.danym.stockinvestingapp.data.StockEntity
 import com.danym.stockinvestingapp.model.StockData
 import com.danym.stockinvestingapp.network.getStockData2
 import com.danym.stockinvestingapp.utility.formatter
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -19,52 +20,57 @@ import retrofit2.Response
 import java.time.LocalDate
 
 class StockViewModel : ViewModel() {
+    private fun isDeviceConnectedToNetwork(context: Context): Boolean {
+        val cm: ConnectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val actNw =
+            cm.getNetworkCapabilities(cm.activeNetwork)
+         return when {
+            actNw?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true -> true
+            actNw?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true -> true
+            actNw?.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) == true -> true
+            else -> false
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
     fun getStockPrices(
-        context: Context,
-        symbol: String,
-        lastNDays: Int,
-        callback: (prices: List<Double>) -> Unit
+        context: Context, symbol: String, lastNDays: Int, callback: (prices: List<Double>) -> Unit
     ) {
         callback(emptyList())
         val room = AppDatabase.getInstance(context)
+        val deviceIsConnectedToNetwork = isDeviceConnectedToNetwork(context)
 
-        val cm: ConnectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo: NetworkInfo? = cm.activeNetworkInfo
-        val deviceIsConnectedToNetwork: Boolean = networkInfo?.isConnected == true
-        Log.i("info", "Device connectivity: $deviceIsConnectedToNetwork")
 
         if (!deviceIsConnectedToNetwork) {
             GlobalScope.launch(Dispatchers.IO) {
                 val result = room.stockDao().getLastNStockData(symbol, lastNDays)
                 Log.i(
                     "info",
-                    "Using Room to get ${result?.size} results for $symbol"
+                    "Using Room to get ${result?.size} results for $symbol. Device connectivity: $deviceIsConnectedToNetwork"
                 )
                 callback(result?.map { it.price } ?: emptyList())
             }
         } else {
             getStockData2(symbol, lastNDays, object : Callback<StockData> {
                 override fun onResponse(
-                    call: Call<StockData>,
-                    response: Response<StockData>
+                    call: Call<StockData>, response: Response<StockData>
                 ) {
                     val body = response.body()
                     Log.i(
                         "info",
-                        "Using Retrofit to get ${body?.historical?.size} results for ${body?.symbol}"
+                        "Using Retrofit to get ${body?.historical?.size} results for ${body?.symbol}. Device connectivity: $deviceIsConnectedToNetwork"
                     )
                     if (body?.historical != null && body.historical.isNotEmpty()) {
                         GlobalScope.launch(Dispatchers.IO) {
                             body.historical.forEach {
-                                room.stockDao()
-                                    .insertStock(
-                                        StockEntity(
-                                            body.symbol,
-                                            it.close,
-                                            LocalDate.parse(it.date, formatter)
-                                        )
+                                room.stockDao().insertStock(
+                                    StockEntity(
+                                        body.symbol,
+                                        it.close,
+                                        LocalDate.parse(it.date, formatter)
                                     )
+                                )
                             }
                         }
                         callback(body.historical.map { it.close })
